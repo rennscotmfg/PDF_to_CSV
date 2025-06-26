9c7769f8adf2e24e5306a65d5c71daf3ef62f8e8
Tai Fayngold <afayngold@rennscot.com>
2025-06-26 09:40:51 -0400

    Fixed gunicorn config

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,10 +1,14 @@
 # gunicorn_conf.py
 
-# Bind to all interfaces on port 5000
 bind = "0.0.0.0:443"
 
-# Only 1 worker process is probably enough for light usage
-workers = 1
+workers = 2
+
+threads = 4
+
+worker_class = "gthread"
+
+worker_tmp_dir = "/dev/shm"
 
 certfile = "cert.pem"
 keyfile = "key.pem"
